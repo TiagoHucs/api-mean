5a0f73fc6de50313d97e813baf6ca97d53c0e6aa
Tiago S. Hucs <tiagohucs@gmail.com>
2019-12-03 09:32:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,23 +13,18 @@ const AuthController = require('./controllers/AuthController');
 const authMiddleware = require('./middlewares/authMiddleware');
 
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 
-//SOLUCAO nao carregara .js .css etc...
-app.use(express.static(__dirname + '/public'));
+app.use("/", express.static(path.join(__dirname, "public")));
 
-// app.get('/*', (req,res)=>{
-//     res.sendFile('/public/index.html', { root: __dirname });
-// }); 
-
-app.get('/*', (req,res)=> res.sendFile(path.join(__dirname))); 
-
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
  
-
 app.post('/auth',validator.body(AuthValidator),AuthController.store);
 app.post('/usuarios', validator.body(UsuarioValidator) ,UsuarioController.store);
 
-//app.use(authMiddleware); //protege rotas abaixo
+app.use(authMiddleware); //protege rotas abaixo
 
 app.use('/posts',PostRoutes);
 app.use('/comentarios',ComentarioRoutes);
